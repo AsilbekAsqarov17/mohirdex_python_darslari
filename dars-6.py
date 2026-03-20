81f3e2f186aa6875ab2076dcb000fcee01c543a6
AsilbekAsqarov17 <asilbek.asqarov.2023@gmail.com>
2026-03-20 08:43:34 +0500

    6 dars uy vazifasi

---
+++ dars-6.py
@@ -0,0 +1,22 @@
+#Asilbek Asqarov
+#Mohirdev python-darslari 
+#dars-6
+
+#task1
+number = int(input("Istalgan sonni kiriting: ")) 
+print(f"{number} ning kvadrati {number**2} ga teng")
+print(f"{number} ning kubi {number**3} ga teng")
+
+#task2
+age = int(input("Yoshingiz nechida? "))
+born_year = 2026 - age
+print(f"Siz {born_year} da tug'ilgansiz")
+
+#task3
+num1 = float(input("Birinchi sonni kiriting: ")) 
+num2 = float(input("Ikkinchi sonni kiriting: "))
+
+print(f"{num1} + {num2} = {num1+num2}")
+print(f"{num1} - {num2} = {num1-num2}")
+print(f"{num1} / {num2} = {num1/num2}")
+print(f"{num1} * {num2} = {num1*num2}")
