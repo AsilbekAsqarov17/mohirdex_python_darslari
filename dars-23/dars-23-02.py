73aca07b3f46dc059630fdb69b42c0d219c6e56e
AsilbekAsqarov17 <asilbek.asqarov.2023@gmail.com>
2026-03-25 16:20:26 +0500

    23 dars uy vazifasi

---
+++ dars-23/dars-23-02.py
@@ -0,0 +1,8 @@
+#Asilbek Asqarov
+#Mohirdev python-darslari
+#dars-23
+
+import avto_info_mod # avto_info_mod faylini (modulini) chaqiramiz
+
+avto1 = avto_info_mod.avto_info("GM", "Malibu", "Qora", "avto", 2020,40000)
+avto_info_mod.info_print(avto1)
